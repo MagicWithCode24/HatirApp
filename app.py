e2f3223c413ff8dc4f1123e6b9086922d8267f16
MagicWithCode24 <137384554+MagicWithCode24@users.noreply.github.com>
2025-07-21 18:24:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
+# Ana sayfa
 @app.route('/')
-def serve_html():
-    return send_from_directory('.', 'index.html')
+def home():
+    return render_template('index.html')
 
-if __name__ == '__main__':
+# Diğer sayfalar
+@app.route('/ana')
+def ana():
+    return render_template('ana.html')
+
+@app.route('/son')
+def son():
+    return render_template('son.html')
+
+# Statik dosyalar (CSS, JS)
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static'), filename)
+
+if __name__ == "__main__":
     app.run(debug=True)
