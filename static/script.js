89481c1deaa8ba589854afae204b8c12bdc57765
MagicWithCode24 <137384554+MagicWithCode24@users.noreply.github.com>
2025-08-22 16:14:13 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -64,7 +64,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 const previewArea = document.getElementById("audioPreview");
                 previewArea.innerHTML = "";
                 const audio = document.createElement("audio");
-                audio.controls = true;
+                
+                // Buradaki "controls" özelliğini kaldırdım
+                // audio.controls = true; 
+                
                 audio.src = audioUrl;
                 const label = document.createElement("p");
                 label.textContent = "Kaydınız:";
